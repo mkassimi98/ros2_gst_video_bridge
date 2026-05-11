fe488e825ce4c15e718888c7e08723d49cf0e2a9
jcurado <jcurado@catec.aero>
2026-05-11 09:07:05 +0200

    Add support to work as composable node

--- ros2_gst_video_bridge/src/gst_video_bridge_node.cc
+++ ros2_gst_video_bridge/src/gst_video_bridge_node.cc
@@ -3,9 +3,13 @@
 
 #include "ros2_gst_video_bridge/gst_video_bridge_node.hpp"
 
+#include <rclcpp_components/register_node_macro.hpp>
+
 namespace ros2_gst_video_bridge {
 
 // Implementations are split across dedicated translation units to keep files
 // focused and below the project line-limit policy.
 
 } // namespace ros2_gst_video_bridge
+
+RCLCPP_COMPONENTS_REGISTER_NODE(ros2_gst_video_bridge::GstVideoBridgeNode)
